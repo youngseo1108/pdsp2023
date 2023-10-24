c44edd28f353ba8ca033598579ff92138392677a
Youngseo <0youngseo1108@gmail.com>
2023-10-24 23:50:41 +0200

    created a docker container

---
+++ frontend/src/config.R
@@ -0,0 +1,14 @@
+###################################################################################################
+# Author: xx
+# Project: xx
+# File: config.R
+# Description: configuration file for session inputs
+###################################################################################################
+# Input: 
+# Output: main inputs to be used in GLOBAL.env
+###################################################################################################
+
+# external ip and port for calling api's in front_end
+# external_ip = "35.240.13.23"
+external_ip = "0.0.0.0"
+port = "8080"
---
+++ frontend/src/run_shiny.R
@@ -0,0 +1,34 @@
+run_shiny_front <- function(external_ip,port){
+    
+options(shiny.host = "0.0.0.0", shiny.port = 3838)
+ui <- fluidPage(
+  # Add a sidebar layout to the application
+  sidebarLayout(
+    # Add a sidebar panel around the text and inputs
+    sidebarPanel(
+      h4("Input parameters for sum"),
+      numericInput(inputId = "x", label ="Select first number", value = 5, min = 0, max = 20),
+      numericInput(inputId = "y", label ="Select second number", value = 3, min = 0, max = 30)
+    ),
+    # Add a main panel around the plot and table
+    mainPanel(
+      textOutput(outputId = "sum")
+    )
+  )
+)
+
+server <- function(input, output, session, e = external_ip, p =  port) {
+  output$sum <- renderText({
+    # GET request from an API
+    base = paste0("http://", e,":", p, "/")
+    r <- httr::GET(url= base,
+                        path="get_sum",
+                        query=list(x=input$x,
+                        y= input$y), verbose()
+    )
+    fromJSON(content(r, "text"))
+  })
+}
+
+shinyApp(ui, server)
+}
